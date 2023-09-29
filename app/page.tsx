0601bb6781fefe326c951ba609fa1fb34b3a9d24
Alejandro <alejandroaperez1994g@gmail.com>
2023-09-29 14:28:26 +0200

    :sparkles: added logout link

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
 import styles from "./page.module.css"
 import Image from "next/image";
+import {getSession} from '@auth0/nextjs-auth0';
 
-export default function Home() {
+async function Home() {
+    const session = await getSession();
     return (
         <main className={styles.container}>
             <Image src={"/assets/images/hero-section.png"} alt={"hero"} width={600} height={600}/>
+
+            <h1>Welcome {session?.user.name}</h1>
+            <a href="/api/auth/logout">Logout</a>
             <h1 className={styles.title}>Rick & Morty Next App</h1>
         </main>
     )
 }
+
+export default Home
